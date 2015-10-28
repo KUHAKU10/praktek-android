ab9fb177096817f1fe846b438c944293e84c2d71
ZERO0611 <wahid.smkn10@gmail.com>
2015-10-28 16:02:07 +0700

    Wahid Jamaalii As Shidqi, Absen 31

---
+++ crud/db_config.php
@@ -0,0 +1,11 @@
+[code lang="php"]
+
+/*
+*properti untuk konek ke db
+*/
+
+define('DB_USER',"root"); //db user
+define('DB_PASSWORD',"silvi"); //db password
+define('DB_DATABASE',"pendaftaran"); //nama db
+define('DB_SERVER',"localhost"); //db server
+[/code]
